f2b2b0d00da48e75016d78726883d1cb588f4c49
DanielMMaciel01 <danielmmacielti@gmail.com>
2025-02-24 20:05:50 -0300

    Projeto Final

--- api/src/main/java/br/com/screenmatch/api/ApiApplication.java
+++ api/src/main/java/br/com/screenmatch/api/ApiApplication.java
@@ -1,13 +1,25 @@
 package br.com.screenmatch.api;
 
+import br.com.screenmatch.api.principal.Principal;
+import br.com.screenmatch.api.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiApplication {
+public class ApiApplication implements CommandLineRunner {
+
+	@Autowired
+	private SerieRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal(repository);
+		principal.exibeMenu();
+	}
 }
